0f38e2b4bd62dff7cc653f0e41fa5bb556d5a8bf
santiago <santithevenetvalles@gmail.com>
2023-10-11 10:12:06 +0200

    Post request for post View

--- post/api.py
+++ post/api.py
@@ -3,10 +3,24 @@ from django.shortcuts import render
 from .models import Post
 from .serializers import PostSerializer
 from rest_framework.decorators import api_view
-
+from .forms import PostForms
 
 @api_view(['GET'])
 def post_list(request):
     post = Post.objects.all()
     serializer = PostSerializer(post, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['POST'])
+def post_create(request):
+    form = PostForms(request.data)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.created_by = request.user
+        post.save()
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data, safe=False)
+    return JsonResponse({
+        'error': 'something went wrong'
+    })
