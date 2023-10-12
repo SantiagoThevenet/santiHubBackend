64b0c7e3ff264bf247cf794e3b3f6eaf9f20d193
santiago <santithevenetvalles@gmail.com>
2023-10-12 10:44:58 +0200

    Search functionality and search results page

--- post/api.py
+++ post/api.py
@@ -1,15 +1,34 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+
 from .models import Post
+from account.models import User
+
 from .serializers import PostSerializer
+from account.serializers import UserSerializer
+
 from rest_framework.decorators import api_view
 from .forms import PostForms
 
+
 @api_view(['GET'])
 def post_list(request):
-    post = Post.objects.all()
-    serializer = PostSerializer(post, many=True)
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['GET'])
+def post_list_profile(request, id):
+    user = User.objects.get(pk=id)
+    posts = Post.objects.filter(created_by=id)
+
+    posts_serializer = PostSerializer(posts, many=True)
+    user_serializer = UserSerializer(user)
+    return JsonResponse({
+        'posts': posts_serializer.data,
+        'user': user_serializer.data
+    }, safe=False)
 
 
 @api_view(['POST'])
