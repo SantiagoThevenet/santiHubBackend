bdf56dc1a21bdd678a1faac48928bfe67f36b556
santiago <santithevenetvalles@gmail.com>
2023-10-17 22:14:17 +0200

    Create comment functionality

--- post/models.py
+++ post/models.py
@@ -1,7 +1,9 @@
 import uuid
+
 from django.db import models
-from account.models import User
 from django.utils.timesince import timesince
+
+from account.models import User
 
 
 class Like(models.Model):
@@ -10,11 +12,25 @@ class Like(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    body = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('created_at',)
+    
+    def created_at_formatted(self):
+       return timesince(self.created_at)
+
+
 class PostAttachment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     image = models.ImageField(upload_to='post_attachments')
     created_by = models.ForeignKey(User, related_name='post_attachments', on_delete=models.CASCADE)
-    
+
+
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     body = models.TextField(blank=True, null=True)
@@ -23,6 +39,9 @@ class Post(models.Model):
 
     likes = models.ManyToManyField(Like, blank=True)
     likes_count = models.IntegerField(default=0)
+
+    comments = models.ManyToManyField(Comment, blank=True)
+    comments_count = models.IntegerField(default=0)
 
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
